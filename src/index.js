2de2ec9331b48bce4cce8059f1e9251d89afabc9
Zakariya06 <zakariyakhanamar00@gmail.com>
2025-12-05 10:27:26 +0500

    utils code add classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
-connectDB();
 
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("ERRR::", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database !!!", err);
+  });
